feab242d0f3d8a284046623971e13c4fe4893512
BraedenBenedict <bbenedi2@nd.edu>
2019-04-25 00:40:00 -0400

    final? update to data analysis

--- matlab_analysis/CatchTiming_v3.m
+++ matlab_analysis/CatchTiming_v3.m
@@ -5,6 +5,7 @@
 %Team FrEE SpEEd
 %% Read File, parse data, filter
 clear all
+clf
 
 M1 = [];
 M2 = [];
@@ -15,7 +16,16 @@ M2 = [];
 M = csvread('ontimeearlyontimelateontime.txt');
 %M = csvread('square.txt');
 %M = csvread('feather.txt');
+%M = csvread('settingtheboat.txt');
+%M = csvread('slap.txt');
+%M = csvread('buildonfeather.txt');
+%M = csvread('squarefeathersquare.txt');
 M = M(100:length(M),:); %remove start of data since all zeros
+
+%making sure M1 and M2 are same length
+if mod(length(M),2)==1
+    M = M(1:length(M)-1,:);
+end
 
 for p=1:length(M)
     if (M(p,8)==1)
@@ -45,14 +55,21 @@ xa2 = M2(:,1);
 ya2 = M2(:,2);
 za2 = M2(:,3);
 
+% millis = M(:,11);
+% xg = M(:,4);
+% yg = M(:,5);
+% zg = M(:,6);
+% xa = M(:,1);
+% ya = M(:,2);
+% za = M(:,3);
+
 % Generate time vector
 msPerRead=(millis(1000)-millis(1))/1000;
 readsPerSecond=1000/msPerRead;
 t = linspace(1,length(xg),length(xg))/readsPerSecond;
 
 %smoothing
-ya = movmean(ya,2);
-
+% ya = movmean(ya,40);
 
 %had to make this change
 zg = -zg;
@@ -64,7 +81,8 @@ xg2=-xg2;
 figure(1)
 hold on
 grid on
-axis([0, max(t), -2-5, 2])
+ axis([0, max(t), -2-5, 2])
+%axis([0, max(t), -2, 2])
 plot(t, ya/10, 'k-')
 plot(t, zg, 'b-')
 plot(t, xg/10, 'g-')
@@ -83,13 +101,14 @@ ready4NewCatch=0;
 timeChange=0;
 featheringState=0; 
 featherTimer=0;
-reSquaredForCatch=1;
+%reSquaredForCatch=1;
 readsPerStrokeCount = 0;
 secsPerStroke = 3;
 featherOrSquare=1; %assume starting on the square
+sharpDipZg=0;
+firstCatch = 0;
 
 %% Input data and analyze
-%On plot, red line is catch, magenta line is feather, cyan line is square
 for i=1:length(t)
     
     %featheringState = whether feathering is happening
@@ -100,7 +119,7 @@ for i=1:length(t)
     %feather, theres a peak in both xg and ya there.
     
     %State machine for featherOrSquare
-    if(xg(i)<-5 && ya(i)<10 && featherOrSquare==1)
+    if(xg(i)<-9 && ya(i)<10 && featherOrSquare==1)
         featheringState=1;
         featherOrSquare=0;
         featherTimer=0;
@@ -108,10 +127,10 @@ for i=1:length(t)
         plot([t(i),t(i)],[-8,6],'m-')
         drawnow
         numFeathers=numFeathers+1;
-    elseif(xg(i)>0.8 && ya(i)>6 && zg(i)>0.8 && featherOrSquare==0)
+    elseif(xg(i)>2 && ya(i)>6 && zg(i)>0.8 && featherOrSquare==0)
         featheringState=1;
         featherOrSquare=1;
-        reSquaredForCatch=1; %for catch state machine
+        %reSquaredForCatch=1; %for catch state machine
         pause(0.1)
         plot([t(i),t(i)],[-8,6],'c-')
         drawnow
@@ -132,128 +151,160 @@ for i=1:length(t)
     %State machine for catches    
     if(ready4NewCatch==0) %not ready for catch
         timeChange=timeChange+1;
-        if(timeChange>readsPerSecond*secsPerStroke*0.6)
+        if(timeChange>readsPerSecond*secsPerStroke*0.5)
             ready4NewCatch=1;
             timeChange=0;
         end
+        sharpDipZg = 0;
         
     elseif(ready4NewCatch==1) %ready for catch
-        %criteria depend on if we're feathering or not
+
+        %NEW! Identifies if a sharp dip has occurred in zg which is indicative
+        %of the global minimum of a stroke
+        if(zg(i)<-0.4 && ((zg(i)-zg(i-3))<-0.1) )
+            sharpDipZg=1;
+        end
+
         
-        %Since **currently** the criteria are pretty much the same for both
-        %while feathering and not feathering, we could probably optimize by
-        %first checking zg, then checking the rest.
-        if(featheringState==1 && zg(i)<-0.4 && zg(i)>zg(i-1)&&reSquaredForCatch==1 )
+        if(featheringState==1 && zg(i-3)<-0.45 && zg(i)>zg(i-1) && zg(i-1)>=zg(i-2) && zg(i-2)>=zg(i-3)...
+                &&sharpDipZg==1) % && reSquaredForCatch==1)
              pause(0.1)
              plot([t(i),t(i)],[-8,6],'r-')
              drawnow
              ready4NewCatch=0;
              numCatches=numCatches+1;
-             secsPerStroke = readsPerStrokeCount/readsPerSecond
+             
+             if firstCatch~=0
+                 secsPerStroke = readsPerStrokeCount/readsPerSecond
+             end
              readsPerStrokeCount = 0;
+             firstCatch=1;
 
-            
-        elseif(featheringState==0 && zg(i)<-0.4 && zg(i)>zg(i-1))
+
+        elseif(featheringState==0 && zg(i-3)<-0.45 && zg(i)>zg(i-1) && zg(i-1)>=zg(i-2) && zg(i-2)>=zg(i-3) ...
+                && (abs(ya(i-2)-ya(i-3)+abs(ya(i-1)-ya(i-2))) > 1.0 )...
+                && sharpDipZg==1)    
              pause(0.1) 
-             plot([t(i),t(i)],[-8,6],'r-')
+             plot([t(i),t(i)],[-8,6],'r--')
              drawnow
-             ready4NewCatch=0;
+             ready4NewCatch=0; 
              numCatches=numCatches+1;
-             secsPerStroke = readsPerStrokeCount/readsPerSecond
+             
+             if firstCatch~=0
+                 secsPerStroke = readsPerStrokeCount/readsPerSecond
+             end
              readsPerStrokeCount = 0;
-             reSquaredForCatch=0;
+             firstCatch=1;
+             
+             %reSquaredForCatch=0;
         end   
     end
 
 featheringStateRecord(i)=featheringState; %saves current featheringState
 readsPerStrokeCount = readsPerStrokeCount+1;
 
-
 end
 
-zg=zg2;
-ya=ya2;
-xg=xg2;
-for i=1:length(t)
-    
-    %featheringState = whether feathering is happening
-    %featherOrSquare = 0 blade is parallel to water, 1 blade is
-    %perpendicular to water
-    
-    %issue is that the square up is being seen immediately after the
-    %feather, theres a peak in both xg and ya there.
-    
-    %State machine for featherOrSquare
-    if(xg(i)<-5 && ya(i)<10 && featherOrSquare==1)
-        featheringState=1;
-        featherOrSquare=0;
-        featherTimer=0;
-        pause(0.1)
-        plot([t(i),t(i)],[-8,6],'m--')
-        drawnow
-        numFeathers=numFeathers+1;
-    elseif(xg(i)>0.8 && ya(i)>6 && zg(i)>0.8 && featherOrSquare==0)
-        featheringState=1;
-        featherOrSquare=1;
-        reSquaredForCatch=1; %for catch state machine
-        pause(0.1)
-        plot([t(i),t(i)],[-8,6],'c--')
-        drawnow
-        numSquares=numSquares+1;
-    end
-    
-    
-    if(featheringState==1)
-        featherTimer=featherTimer+1; %counts reads since last feathering motion 
-        if(featherTimer>(readsPerSecond*secsPerStroke*1.4))
-            featheringState=0; %no longer feathering
-            featherOrSquare=1;
-            featherTimer=0;
-        end
-    end
-    
-    
-    %State machine for catches    
-    if(ready4NewCatch==0) %not ready for catch
-        timeChange=timeChange+1;
-        if(timeChange>readsPerSecond*secsPerStroke*0.6)
-            ready4NewCatch=1;
-            timeChange=0;
-        end
-        
-    elseif(ready4NewCatch==1) %ready for catch
-        %criteria depend on if we're feathering or not
-        
-        %Since **currently** the criteria are pretty much the same for both
-        %while feathering and not feathering, we could probably optimize by
-        %first checking zg, then checking the rest.
-        if(featheringState==1 && zg(i)<-0.4 && zg(i)>zg(i-1)&&reSquaredForCatch==1 )
-             pause(0.1)
-             plot([t(i),t(i)],[-8,6],'y--')
-             drawnow
-             ready4NewCatch=0;
-             numCatches=numCatches+1;
-             secsPerStroke = readsPerStrokeCount/readsPerSecond
-             readsPerStrokeCount = 0;
-
-            
-        elseif(featheringState==0 && zg(i)<-0.4 && zg(i)>zg(i-1))
-             pause(0.1) 
-             plot([t(i),t(i)],[-8,6],'y--')
-             drawnow
-             ready4NewCatch=0;
-             numCatches=numCatches+1;
-             secsPerStroke = readsPerStrokeCount/readsPerSecond
-             readsPerStrokeCount = 0;
-             reSquaredForCatch=0;
-        end   
-    end
-
-featheringStateRecord(i)=featheringState; %saves current featheringState
-readsPerStrokeCount = readsPerStrokeCount+1;
 
 
-end
+% zg=zg2;
+% ya=ya2;
+% xg=xg2;
+% for i=1:length(t)
+%     
+%     %featheringState = whether feathering is happening
+%     %featherOrSquare = 0 blade is parallel to water, 1 blade is
+%     %perpendicular to water
+%     
+%     %issue is that the square up is being seen immediately after the
+%     %feather, theres a peak in both xg and ya there.
+%     
+%     %State machine for featherOrSquare
+%     if(xg(i)<-9 && ya(i)<10 && featherOrSquare==1)
+%         featheringState=1;
+%         featherOrSquare=0;
+%         featherTimer=0;
+%         pause(0.1)
+%         %plot([t(i),t(i)],[-8,6],'m-')
+%         drawnow
+%         numFeathers=numFeathers+1;
+%     elseif(xg(i)>2 && ya(i)>6 && zg(i)>0.8 && featherOrSquare==0)
+%         featheringState=1;
+%         featherOrSquare=1;
+%         reSquaredForCatch=1; %for catch state machine
+%         pause(0.1)
+%         %plot([t(i),t(i)],[-8,6],'c-')
+%         drawnow
+%         numSquares=numSquares+1;
+%     end
+%     
+%     
+%     if(featheringState==1)
+%         featherTimer=featherTimer+1; %counts reads since last feathering motion 
+%         if(featherTimer>(readsPerSecond*secsPerStroke*1.4))
+%             featheringState=0; %no longer feathering
+%             featherOrSquare=1;
+%             featherTimer=0;
+%         end
+%     end
+%     
+%     
+%     %State machine for catches    
+%     if(ready4NewCatch==0) %not ready for catch
+%         timeChange=timeChange+1;
+%         if(timeChange>readsPerSecond*secsPerStroke*0.5)
+%             ready4NewCatch=1;
+%             timeChange=0;
+%         end
+%         sharpDip = 0;
+%         
+%     elseif(ready4NewCatch==1) %ready for catch
+%         %criteria depend on if we're feathering or not
+%         if(zg(i)<-0.4 && ((zg(i)-zg(i-3))<-0.1) )
+%             sharpDip=1;
+%         end
+%         %Since **currently** the criteria are pretty much the same for both
+%         %while feathering and not feathering, we could probably optimize by
+%         %first checking zg, then checking the rest.
+%         if(featheringState==1 && zg(i-3)<-0.45 && zg(i)>zg(i-1) && zg(i)>zg(i-2) && zg(i-1)>=zg(i-2) && zg(i-2)>=zg(i-3)...
+%                 &&sharpDip==1) % && reSquaredForCatch==1)
+%              pause(0.1)
+%              plot([t(i),t(i)],[-8,6],'r-')
+%              drawnow
+%              ready4NewCatch=0;
+%              numCatches=numCatches+1;
+%              
+%              if firstCatch~=0
+%                  secsPerStroke = readsPerStrokeCount/readsPerSecond
+%              end
+%              readsPerStrokeCount = 0;
+%              firstCatch=1;
+% 
+% 
+%         elseif(featheringState==0 && zg(i-3)<-0.45 && zg(i)>zg(i-1) && zg(i)>zg(i-2) && zg(i-1)>=zg(i-2) && zg(i-2)>=zg(i-3) ...
+%                 && (abs(ya(i-2)-ya(i-3)+abs(ya(i-1)-ya(i-2))) > 1.0 )...
+%                 && sharpDip==1)    
+%         %elseif(featheringState==0 && zg(i)<-0.4 && zg(i)>zg(i-1) && zg(i)>zg(i-2))
+%              pause(0.1) 
+%              plot([t(i),t(i)],[-8,6],'r--')
+%              drawnow
+%              ready4NewCatch=0; 
+%              numCatches=numCatches+1;
+%              
+%              if firstCatch~=0
+%                  secsPerStroke = readsPerStrokeCount/readsPerSecond
+%              end
+%              readsPerStrokeCount = 0;
+%              firstCatch=1;
+%              
+%              reSquaredForCatch=0;
+%         end   
+%     end
+% 
+% featheringStateRecord(i)=featheringState; %saves current featheringState
+% readsPerStrokeCount = readsPerStrokeCount+1;
+% 
+% end
 
 legend('Y accel/10', 'Z gyro', 'X gyro/10');
 plot(t,featheringStateRecord,'k')
